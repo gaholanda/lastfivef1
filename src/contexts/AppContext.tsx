9615561f054b37294586109247ae4c172c853995
Glauber Holanda <glauberholanda10@gmail.com>
2022-07-24 20:14:06 -0300

    refactor: remove points model

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,7 +1,6 @@
 import { createContext, ReactNode } from "react";
 import { useRequest } from "../hooks/useRequest";
 import { Teams, Drivers, Races } from "../models";
-import { Points } from "../models/points";
 
 interface AppProviderProps {
   children: ReactNode;
@@ -11,7 +10,6 @@ interface AppContextData {
   drivers: Drivers;
   teams: Teams;
   races: Races;
-  points: Points;
 }
 
 export const AppContext = createContext({} as AppContextData);
@@ -20,7 +18,6 @@ export function AppProvider({ children }: AppProviderProps) {
   const { data: drivers = {} } = useRequest<Drivers>("drivers.json");
   const { data: teams = {} } = useRequest<Teams>("teams.json");
   const { data: races = {} } = useRequest<Races>("races.json");
-  const { data: points = {} as Points } = useRequest<Points>("points.json");
 
   return (
     <AppContext.Provider
@@ -28,7 +25,6 @@ export function AppProvider({ children }: AppProviderProps) {
         drivers,
         teams,
         races,
-        points
       }}
     >
       {children}
