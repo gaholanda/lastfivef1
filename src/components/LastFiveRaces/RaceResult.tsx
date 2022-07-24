53eab2b80d3c55da91921cef327797d67d562807
Glauber Holanda <glauberholanda10@gmail.com>
2022-07-24 20:02:17 -0300

    refactor: add pole to dnf

--- src/components/LastFiveRaces/RaceResult.tsx
+++ src/components/LastFiveRaces/RaceResult.tsx
@@ -28,7 +28,7 @@ export function RaceResult({
     height: "25px",
     lineHeight: "25px",
     fontSize: "0.9rem",
-  }
+  };
 
   return (
     <div className="d-flex flex-grow-1 flex-column align-items-center">
@@ -42,27 +42,25 @@ export function RaceResult({
           />
           <div className="d-flex flex-column align-items-center mt-2">
             <div className="flex-grow-1 text-center">
-              {position === 0 && <p className="m-0">🚫</p>}
-              {position > 0 && (
-                <p className="m-0">
-                  <strong
-                    className={setPositionColor(position)}
-                    style={{
-                      fontSize: "1.5rem",
-                    }}
-                  >
-                    {position}
-                    {pole && 
-                      <sup 
-                        className="d-inline-block pole--background rounded-circle" 
-                        style={{ 
-                          width: '8px', 
-                          height: '8px'}} 
-                      />
-                    }
-                  </strong>
-                </p>
-              )}
+              <p className="m-0">
+                <strong
+                  className={setPositionColor(position)}
+                  style={{
+                    fontSize: "1.5rem",
+                  }}
+                >
+                  {position === 0 ? "🚫" : position}
+                  {pole && (
+                    <sup
+                      className="d-inline-block pole--background rounded-circle"
+                      style={{
+                        width: "8px",
+                        height: "8px",
+                      }}
+                    />
+                  )}
+                </strong>
+              </p>
               {race_points > 0 && (
                 <p className="m-0">
                   {race_points > 0 && "+"}
@@ -74,13 +72,17 @@ export function RaceResult({
               <p
                 className="m-0 mt-1 fastest-lap rounded-circle d-inline-block text-center"
                 style={FLapSprintPointsStyle}
-              ><strong>+1</strong></p>
+              >
+                <strong>+1</strong>
+              </p>
             )}
             {sprint_points && (
               <p
                 className="m-0 mt-1 sprint rounded-circle d-inline-block text-center"
                 style={FLapSprintPointsStyle}
-              ><strong>+{sprint_points}</strong></p>
+              >
+                <strong>+{sprint_points}</strong>
+              </p>
             )}
           </div>
         </div>
